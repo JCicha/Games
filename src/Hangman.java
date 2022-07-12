06431330a0c5b76fe2c32851085f45affc6650a5
Joanna Cicha <jcicha@deloittece.com>
2022-07-12 17:24:41 +0200

    Dodawanie słowa do puli wyrazów

--- src/Hangman.java
+++ src/Hangman.java
@@ -1,13 +1,37 @@
-import java.util.Random;
+import java.io.FileWriter;
 import java.util.Scanner;
 
 public class Hangman {
 
     public static void main(String[] args) {
-        System.out.println("Witaj w grze wisielec! Co chcesz zrobic? (Wpisz numer operacji)\n " +
-                "1. Zagrac!\n 2. Dodac slowo do puli wyrazow.");
-        Scanner in = new Scanner(System.in);
-        String userChoice = in.nextLine();
-        System.out.println(userChoice);
+        while (true) {
+            System.out.println("Witaj w grze wisielec! Co chcesz zrobic? (Wpisz numer operacji)\n " +
+                    "1. Zagrac!\n 2. Dodac slowo do puli wyrazow. \n 3. Zakonczyc program.");
+            Scanner in = new Scanner(System.in);
+            int userChoice = Integer.parseInt(in.nextLine());
+            System.out.println(userChoice);
+            switch(userChoice) {
+                case 2: {
+                    System.out.println("Podaj słowo, które chcesz dodać do puli wyrazów. \n");
+                    Scanner scan = new Scanner(System.in);
+                    String userWord = scan.nextLine();
+                    System.out.println(userWord);
+                    try {
+                        FileWriter output = new FileWriter("Passwords.txt");
+                        output.write(userWord);
+                        output.close();
+                    } catch (Exception e) {
+                        e.getStackTrace();
+                    }
+                }
+                case 3: {
+                    break;
+                }
+                default: {
+                    System.out.println("Niepoprawnie wybrana opcja");
+                }
+            }
+        }
     }
+
 }
